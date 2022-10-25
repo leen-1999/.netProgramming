060e1949f0a0425936768913801415183f07e5a3
leen-1999 <80642456+leen-1999@users.noreply.github.com>
2022-10-25 19:42:40 +0300

    upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.textLastName = new System.Windows.Forms.TextBox();
+            this.textAddress = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Address";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(148, 26);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(170, 22);
+            this.txtId.TabIndex = 4;
+            this.txtId.Text = "1";
+            this.txtId.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(148, 67);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(170, 22);
+            this.textName.TabIndex = 5;
+            // 
+            // textLastName
+            // 
+            this.textLastName.Location = new System.Drawing.Point(148, 125);
+            this.textLastName.Name = "textLastName";
+            this.textLastName.Size = new System.Drawing.Size(170, 22);
+            this.textLastName.TabIndex = 6;
+            // 
+            // textAddress
+            // 
+            this.textAddress.Location = new System.Drawing.Point(148, 179);
+            this.textAddress.Name = "textAddress";
+            this.textAddress.Size = new System.Drawing.Size(170, 22);
+            this.textAddress.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Add Customer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(208, 256);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 34);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "List Customer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(49, 331);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 34);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Add Employee";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(216, 339);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(464, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Cust ID";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(801, 32);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Emp ID";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(532, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(218, 24);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.Click += new System.EventHandler(this.comboBox1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(868, 26);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(218, 24);
+            this.comboBox2.TabIndex = 16;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            this.comboBox2.Click += new System.EventHandler(this.comboBox2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(485, 73);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(627, 196);
+            this.listBox1.TabIndex = 17;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(485, 289);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(627, 41);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Remove Customer";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(485, 353);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(627, 41);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "Remove employee";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(208, 330);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 34);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "List Employee";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1160, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textAddress);
+            this.Controls.Add(this.textLastName);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.TextBox textLastName;
+        private System.Windows.Forms.TextBox textAddress;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Customer> customers = new List<Customer>();
+        List<Employee> employees = new List<Employee>();
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Customer customer = new Customer();
+            customer.Id = Convert.ToInt32(txtId.Text);
+            customer.FirstName = textName.Text;
+            customer.LastName = textLastName.Text;
+            customer.Address = textAddress.Text;
+
+            customers.Add(customer);
+            clearTxtBox();
+
+        }
+        public void clearTxtBox()
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is TextBox)
+                    c.Text = " ";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Employee employee = new Employee();
+            employee.Id = Convert.ToInt32(txtId.Text);
+            employee.FirstName = textName.Text;
+            employee.LastName = textLastName.Text;
+            employee.Address = textAddress.Text;
+            employees.Add(employee);
+            clearTxtBox();
+          
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (Employee employee in employees)
+                listBox1.Items.Add(employee.FirstName + " " + employee.LastName);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (IPerson x in customers)
+                listBox1.Items.Add(x.FirstName + " " + x.LastName);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+       
+
+            string text = listBox1.GetItemText(listBox1.SelectedItem);
+
+
+
+            for (int i = 0; i < customers.Count; i++)
+
+                if (customers[i].FirstName + " " + customers[i].LastName == text)
+
+                    customers.Remove(customers[i]);
+
+
+
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+            string text = listBox1.GetItemText(listBox1.SelectedItem);
+
+
+
+            for (int i = 0; i < employees.Count; i++)
+
+                if (employees[i].FirstName + " " + employees[i].LastName == text)
+
+                    employees.Remove(employees[i]);
+
+
+
+            listBox1.Items.Remove(listBox1.SelectedItem);
+
+        }
+
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            foreach (IPerson x in customers)
+                comboBox1.Items.Add(x.Id);
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+         
+            listBox1.Items.Clear();
+
+            int y = Int32.Parse(comboBox1.Text);
+
+            foreach (IPerson x in customers)
+
+                if (x.Id == y)
+
+                {
+
+                    listBox1.Items.Add(x.FirstName + " " + x.LastName);
+
+                }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            //foreach (Employee employee in employees)
+            //    comboBox2.Items.Add(employee.Id); 
+                
+        }
+        private void comboBox2_Click(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();
+            foreach (IPerson x in employees)
+                comboBox2.Items.Add(x.Id);
+        }
+
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            int y = Int32.Parse(comboBox2.Text);
+
+            foreach (IPerson x in employees)
+
+                if (x.Id == y)
+
+                {
+
+                    listBox1.Items.Add(x.FirstName + " " + x.LastName);
+
+                }
+        }
+    }
+}
